a3d0cf4fcd11582f908abbb46ce135df7809cf82
ARK2306 <aryanreddykalluri@gmail.com>
2025-01-27 17:42:25 -0600

    images fixed

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,13 +1,12 @@
 import { Link } from "react-router-dom";
 import styles from "./Logo.module.css";
-import logoImage from "../public/images/logo.png";
 
 function Logo() {
   return (
     <Link to="/">
       <img
-        src={logoImage} // This will look for logo.png in the public folder
-        alt="drift diaries logo"
+        src="/images/logo.png"
+        alt="DriftDiaries logo"
         className={styles.logo}
       />
     </Link>
--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import * as path from "path"; // Add this import
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/", // Changed from './' to '/'
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "@public": path.resolve(__dirname, "./public"),
     },
   },
   build: {
     outDir: "dist",
     assetsDir: "assets",
     rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, "index.html"),
-      },
       output: {
         assetFileNames: (assetInfo) => {
           let extType = assetInfo.name.split(".").at(1);
