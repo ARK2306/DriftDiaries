bb2dad9d3fe76487bf7310de37059b51021ee9ce
ARK2306 <aryanreddykalluri@gmail.com>
2025-01-27 19:46:55 -0600

    added details for guest

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,11 +2,13 @@ import PageNav from "../components/PageNav";
 import styles from "./Login.module.css";
 
 import AuthForm from "../components/AuthForm";
+import Card from "./card";
 
 export default function Login() {
   return (
     <main className={styles.login}>
       <PageNav />
+      <Card />
       <AuthForm />
     </main>
   );
